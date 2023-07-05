42c4159d43a5ed376677ba8c8a2018a92490680f
woom1441 <woom1441@naver.com>
2023-07-05 17:01:08 +0900

    부동산3

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,18 @@
   <div>
     <h4>{{products[0]}}</h4>
     <p>{{price1[0]}}만원</p>
-    <button v-on:click="신고수++">허위매물신고 </button>
-    <span>신고수 : {{신고수}}</span> 
+    <button @click=increase1>허위매물신고</button>
+    <span>신고수 : {{신고수[0]}}</span> 
   </div>
   <div>
     <h4>{{products[1]}}</h4>
     <p>{{price1[1]}}만원</p> 
+    <button @click=increase2>허위매물신고</button><span>신고수 : {{신고수[1]}}</span>
   </div>  
   <div>
     <h4>{{products[2]}}</h4>
-    <p>{{price1[2]}}만원</p> 
+    <p>{{price1[2]}}만원</p>
+    <button @click=increase3>허위매물신고</button><span>신고수 : {{신고수[2]}}</span> 
   </div>      
 </template>
 
@@ -28,7 +30,18 @@ export default {
       price1:[ 60, 70, 80 ],      
       메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
-      신고수: 0,     
+      신고수: [0, 0, 0,],     
+    }
+  },
+  methods: {
+    increase1(){
+      this.신고수[0]++;
+    },
+    increase2(){
+      this.신고수[1]++;
+    },
+    increase3(){
+      this.신고수[2]++;
     }
   },
   components: {
